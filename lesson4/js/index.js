890899fe368bfee0b5ab59df66c857efbf263322
scarillo77 <car16104@byui.edu>
2021-05-19 15:40:51 -0600

    update

--- lesson4/js/index.js
+++ lesson4/js/index.js
@@ -14,11 +14,10 @@ window.onresize = () => {
   if (window.innerWidth > 760) mainnav.classList.remove("responsive");
 };
 
-const datefield = document.querySelector("date");
+const datefield = document.querySelector("#date");
 
-const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US" , { dateStyle: "full"}).format (
- now
+ date
 );
 
 datefield.textContent = fulldate;
