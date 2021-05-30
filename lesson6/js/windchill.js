241d5b2aa2d97271911abdf724212ffa1209304c
scarillo77 <car16104@byui.edu>
2021-05-29 22:51:03 -0600

    update

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,14 @@
+let wc = windchill + "deg;F";
+
+document.querySelector(".").innerHTML = wc;
+
+let temp = doc.querySelector
+let speed = doc.querySelector
+
+if (meet conditions compound) {
+    call windChill(temp, speed)
+}
+
+function windChill(t, s) {
+    return Math.pow(t,2)
+}
