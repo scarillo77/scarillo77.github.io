ef1467b9ad7d40dd6e24e52b9252abf258f53255
scarillo77 <car16104@byui.edu>
2021-06-05 22:20:03 -0600

    update

--- lesson7/js/index.js
+++ lesson7/js/index.js
@@ -30,3 +30,36 @@ datefield.textContent = fulldate;
 if (date.getDay() == 5) {
   document.querySelector("#banner").style.display = "block";
 }
+
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+const imgOptions = {
+  rootMargin: "0px 0px 50px 0px",
+  threshold: 1,
+};
+
+if ("IntersectionObserver" in window) {
+  const imgObserver = new IntersectionObserver((items) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        imgObserver.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+
+  imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
