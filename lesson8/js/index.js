ef044c6c262ed6af8c80063b129220c00cf8a27d
scarillo77 <car16104@byui.edu>
2021-06-15 22:58:23 -0600

    update

--- lesson8/js/index.js
+++ lesson8/js/index.js
@@ -34,3 +34,11 @@ if (date.getDay() == 5) {
 function adjustRating(rating) {
   document.getElementById("ratingvalue").innerHTML = rating;
 }
+
+function selectResponse() {
+	const s = document.querySelector('#selected')
+	const sel = document.querySelector('#selectbrowser');
+	s.style.display = "block";
+	s.textContent = sel.value;
+	
+}
