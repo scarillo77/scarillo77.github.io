96cb7b4a972d03c3023ecaef4da83cc11501eb60
scarillo77 <car16104@byui.edu>
2021-06-26 00:32:20 -0600

    update

--- lesson9/js/info.js
+++ lesson9/js/info.js
@@ -1,29 +1,23 @@
-const requestURL =
-  "https://byui-cit230.github.io/weather/data/towndata.json";
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
 
-  fetch(requestURL)
+fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
     console.table(jsonObject); // temporary checking for valid response and data parsing
-    const town = jsonObject["town"];
-    for (let i = 0; i < town.length; i++) {
+    const towns = jsonObject["towns"];
+    for (let i = 0; i < towns.length; i++) {
       let card = document.createElement("section");
       let h2 = document.createElement("h2");
       let date = document.createElement("p");
       let place = document.createElement("p");
       let image = document.createElement("img");
 
-      h2.textContent = town[i].name + " " + town[i].motto;
-      date.textContent = "Date of Birth: " + town[i].yearFounded;
-      image.src = town[i].imageurl;
+      h2.textContent = towns[i].name + " " + towns[i].motto;
+      image.src = towns[i].photos;
       image.alt =
-        town[i].name +
-        " " +
-        town[i].motto +
-        " - " +
-        town[i].yearFounded;
+        towns[i].name + " " + towns[i].motto + " - " + towns[i].yearFounded;
 
       card.appendChild(h2);
       card.appendChild(date);
