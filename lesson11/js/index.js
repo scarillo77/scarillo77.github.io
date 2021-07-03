855ae1882b342a45d558b4164b1445d72039cfc1
scarillo77 <car16104@byui.edu>
2021-07-02 22:17:51 -0600

    update

---
+++ lesson11/js/index.js
@@ -0,0 +1,61 @@
+let date = new Date();
+let year = date.getFullYear();
+
+document.querySelector("#theyear").textContent = year;
+
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {
+  if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+};
+
+const datefield = document.querySelector("#date");
+
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  date
+);
+
+datefield.textContent = fulldate;
+
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+const imgOptions = {
+  rootMargin: "0px 0px 50px 0px",
+  threshold: 1,
+};
+
+if ("IntersectionObserver" in window) {
+  const imgObserver = new IntersectionObserver((items) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        imgObserver.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+
+  imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
---
+++ lesson11/js/info.js
@@ -0,0 +1,73 @@
+let date = new Date();
+let year = date.getFullYear();
+
+document.querySelector("#theyear").textContent = year;
+
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {
+  if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+};
+
+const datefield = document.querySelector("#date");
+
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  date
+);
+
+datefield.textContent = fulldate;
+
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const towns = jsonObject["towns"];
+    const weatherTowns = towns.filter(
+      (town) =>
+        town.name == "Soda Springs" ||
+        town.name == "Fish Haven" ||
+        town.name == "Preston"
+    );
+
+    for (let i = 0; i < weatherTowns.length; i++) {
+      let card = document.createElement("div");
+      let h3 = document.createElement("h3");
+      let motto = document.createElement("p");
+      let images = document.createElement("img");
+      let year = document.createElement("p");
+      let population = document.createElement("p");
+
+
+      h3.textContent = weatherTowns[i].name;
+      motto.textContent = weatherTowns[i].motto;
+      images.setAttribute("src", weatherTowns[i].photo);
+      images.setAttribute("alt", "A photo of " + weatherTowns[i].name);
+      year.textContent = "Founded in " + weatherTowns[i].yearFounded;
+      population.textContent =
+        "Population: " + weatherTowns[i].currentPopulation;
+      
+      
+      card.appendChild(h3);
+      card.appendChild(motto);
+      card.appendChild(images);
+      card.appendChild(year);
+      card.appendChild(population);
+     
+      
+
+      document.querySelector(".town").appendChild(card);
+    }
+  });
