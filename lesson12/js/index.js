48ace2470c5e7091c5e7afbe5b1b9575deb5e182
scarillo77 <car16104@byui.edu>
2021-07-20 19:51:22 -0600

    update

--- lesson12/js/index.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('#lastmod').textContent = document.lastModified;
-
-let date = new Date();
-let year = date.getFullYear();
-
-document.querySelector('#theyear').textContent = year;
---
+++ lesson14/js/index.js
@@ -0,0 +1,22 @@
+document.querySelector("#lastmod").textContent = document.lastModified;
+
+let date = new Date();
+let year = date.getFullYear();
+
+document.querySelector("#theyear").textContent = year;
+
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {
+  if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+};
