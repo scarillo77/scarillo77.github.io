a0e6cbdcac849d04069ac0998229f5905cd75f3d
scarillo77 <car16104@byui.edu>
2021-07-21 12:07:29 -0600

    update

--- lesson14/js/index.js
+++ lesson14/js/index.js
@@ -20,3 +20,38 @@ hambutton.addEventListener(
 window.onresize = () => {
   if (window.innerWidth > 760) mainnav.classList.remove("responsive");
 };
+
+// Directory
+const requestURL = "https://scarillo77.github.io/lesson14/json/info.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const business = jsonObject["business"];
+
+    for (let i = 0; i < business.length; i++) {
+      let card = document.createElement("div");
+      let name = document.createElement("h2");
+      let address = document.createElement("p");
+      let images = document.createElement("img");
+      let siteurl = document.createElement("p");
+      let phone = document.createElement("p");
+
+      h2.textContent = business[i].name;
+      address.textContent = "Address:" + business[i].address;
+      phone.textContent = "Phone Number:" + business[i].phone;
+      siteurl.textContent = business[i].currentPopulation;
+      images.setAttribute("src", business[i].photo);
+      images.setAttribute("alt", "A photo of " + business[i].name);
+
+      card.appendChild(h2);
+      card.appendChild(address);
+      card.appendChild(phone);
+      card.appendChild(siteurl);
+      card.appendChild(images);
+
+      document.querySelector(".business").appendChild(card);
+    }
+  });
